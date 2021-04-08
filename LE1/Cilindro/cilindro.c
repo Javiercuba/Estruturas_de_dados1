feb39143a2a95ce15576c1ed1ca82b4a2a72018b
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2021-04-08 19:40:19 -0300

    Cilindro Readme

--- LE1/Cilindro/cilindro.c
+++ LE1/Cilindro/cilindro.c
@@ -13,7 +13,6 @@ int main()
     printf("Atribuir valores de raio e altura manualmente\n");
     atribui_valores(&c, rand() % 100 /* raio*/, rand() % 100 /* altura*/); //ATRIBUINDO OS VALORES ALEATORIAMENTE
     printf("O valor do raio é %.1f, e a altura é %.1f\n", c.raio, c.altura);
-    
     calcula_base(c, &area);    //CALCULO DA BASE POR PASSAGEM DE REFERENCIA
     calcula_lateral(c, &area); //CALCULO DA LATERAL POR PASSAGEM DE REFERENCIA
     calcula_volume(c, &area);  //CALCULO DO VOLUME
